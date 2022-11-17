2b432aad05ac36ce6d6718040a5558b64e552265
CalebLovell <caleblovell1@gmail.com>
2022-11-16 21:29:41 -0500

    update message

--- src/index.ts
+++ src/index.ts
@@ -160,8 +160,4 @@ app.get(`/feed`, async (req, res) => {
 	res.json(posts);
 });
 
-const server = app.listen(3000, () =>
-	console.log(`
-🚀 Server ready at: http://localhost:3000
-⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
-);
+const server = app.listen(3000, () => console.log(`Server ready at: http://localhost:3000`));
